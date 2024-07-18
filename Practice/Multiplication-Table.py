c79e2f8578bd2def9aa76aea0b78e85b932f74b6
Sarthak Bansal <bansalsarthak504@gmail.com>
2024-07-18 10:09:43 +0530

    update

--- Practice/Multiplication-Table.py
+++ Practice/Multiplication-Table.py
@@ -13,7 +13,7 @@ def multiplication_table():
 # Call the function to generate the multiplication table
 multiplication_table()
 
-# This programs prints tables from 2 to 10
+# This program prints tables from 2 to 10
 # If you have any specific questions or need more detailed explanations, feel free to ask!
 # on my LinkedIn- https://www.linkedin.com/in/sarthak-bansal-6744a2256/
 # It will help both of us to grow and learn more stuff in python
