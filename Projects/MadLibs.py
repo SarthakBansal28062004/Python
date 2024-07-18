eda5d790088e802212aaae122a0cd9d599395b9e
Sarthak Bansal <bansalsarthak504@gmail.com>
2024-07-18 10:15:20 +0530

    update

---
+++ Projects/MadLibs.py
@@ -0,0 +1,46 @@
+print("Welcome to MadLibs 🤩")
+print("Let's Start 😇\n")
+
+adjective_1 = input("Enter an Adjective: ")
+adjective_2 = input("Enter another Adjective: ")
+noun_1 = input("Enter a noun: ")
+name_1 = input("Enter a Name: ")
+noun_2 = input("Enter another noun: ")
+verbing_1 = input("Enter a verb ending with '-ing': ")
+place_1 = input("Enter a place: ")
+adjective_3 = input("Enter another adjective: ")
+
+print("\nJust a Few More! Have Patience ☺️")
+
+verbing_2 = input("Enter another verb ending with '-ing': ")
+place_2 = input("Enter another place: ")
+name_2 = input("Enter another name: ")
+adjective_4 = input("Enter another adjective: ")
+
+print("\nYou are just this close! 🤏🏼")
+
+noun_3 = input("Enter another noun: ")
+plural_noun_1 = input("Enter a plural noun: ")
+plural_noun_2 = input("Enter another plural noun: ")
+noun_4 = input("Enter another noun: ")
+
+print("\nJust last two for you! 💪🏻")
+
+name_3 = input("Enter another name: ")
+noun_5 = input("Enter another name: ")
+
+print("\nHere You Go! 🏎️")
+
+madlib_1 = f"Once upon a time, in a {adjective_1} land far away, there was a/an {adjective_2} {noun_1} named {name_1}."
+madlib_2 = f"This {noun_2} had a special talent of {verbing_1}, which made everyone in the {place_1} extremely {adjective_3}."
+madlib_3 = f"One day, while {verbing_2} in {place_2}, {name_2} stumbled upon a/an {adjective_4} {noun_3}."
+madlib_4 = f"This discovery led to an unforgettable adventure filled with {plural_noun_1}, {plural_noun_2}, and lots of {noun_4}s."
+madlib_5 = f"In the end, {name_3} learned that {noun_5} was the true key to happiness."
+
+print(madlib_1)
+print(madlib_2)
+print(madlib_3)
+print(madlib_4)
+print(madlib_5)
+
+print("\nThanks For Playing! 🤗")
